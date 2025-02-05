28ad0da46620129e18120b8b8ea1acfa071bfe97
Elaine Heaney <eheaney837@gmail.com>
2025-02-05 14:05:30 +0000

    delete and edit journal functionality

--- public/manageJournal.js
+++ public/manageJournal.js
@@ -1,6 +1,21 @@
 //The URIs of the Azure endpoint and blob account
+
+//Create Individual Journal - allows user to create their new journal
 CIJ = "https://prod-03.ukwest.logic.azure.com:443/workflows/fe03f09bead94bf2991c35ed44e5f327/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=wSxGq-yTWriVowlNApTuLmCl9OwypgeSLs5oWobdQys";
+//Retrieve All Journals - allows user to view their journals upon registering or logging in
 RAJ = "https://prod-00.uksouth.logic.azure.com:443/workflows/dbebdcf4ca5c4b9c9e991fe436cf1f38/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=4a-KNy3roqCYdg_IYc3xhnFPsW5FtZ1oxJcgtJRWqCs";
+//Retrieve Individual Journal - needed for editing details
+RIJ1 = "https://prod-19.uksouth.logic.azure.com/workflows/4c3c838289dc490f9949823edeb1c51b/triggers/When_a_HTTP_request_is_received/paths/invoke/journal/";
+RIJ2 = "?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=D65TyHGar6T6FWvLZfuZ80Qv8kYoEZBjeQ-3I0Jw2H4";
+
+//Delete Individual Journal 1 + 2 - split so that a unique ID can be placed inbetween when sending a request
+DIJ1 = "https://prod-28.ukwest.logic.azure.com/workflows/991fb177d6c849ca9963052469c7b270/triggers/When_a_HTTP_request_is_received/paths/invoke/delete/journal/";
+DIJ2 = "?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=QxbzXl_1-2hnO_FY_I452LNTgwRyRkWfI2PPTRHcyFs";
+
+//Update Individual Journal 1 + 2 split so that a unique ID can be placed inbetween when sending a request
+UIJ1 = "https://prod-08.ukwest.logic.azure.com/workflows/bf91eab02c924b2592ad484be3178f40/triggers/When_a_HTTP_request_is_received/paths/invoke/update/journal/";
+UIJ2 = "?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=soAytIHb0W3cPy6tS0meZ8e3Rk-vCLF47nUSIu843hQ";
+//Link to blob account where multimedia is stored
 BLOB_ACCOUNT = "https://webjournalstorage.blob.core.windows.net";
 
 //Handlers for button clicks
@@ -12,8 +27,6 @@ $(document).ready(function() {
   }); 
 
    $("#subNewJournal").click(function(){
-
-    console.log();
     //Call the function to add the journal
     submitNewJournal();
     
@@ -68,12 +81,86 @@ function getJournals() {
     // Clear the Journal list div
     $('#JournalList').empty();
 
-    // Append the contents of the items array to the JournalList Div
     $("<ul/>", {
       "class": "my-new-list",
       html: items.join("")
     }).appendTo("#JournalList");
 
 
+    $(".delete-button").on("click", function() {
+      const journalId = $(this).data("id"); // Retrieve the id from the data-id attribute
+      if (confirm("Are you sure you want to delete this Journal? All entries entered will also be deleted!")) {
+        deleteJournal(journalId);
+      }
+    });
+
+    $(".edit-button").on("click", function() {
+      const journalId = $(this).data("id"); 
+      $.getJSON(RIJ1 + journalId + RIJ2, function(data) {
+        $("#editJournalName").val(data.Documents.journalName);
+    
+        $("#EditJournal").show(); 
+        $("#JournalList").hide(); 
+    
+        $("#saveEdit").data("id", journalId); 
+      });
+    });
+    
+    $("#cancelEdit").on("click", function() {
+      $("#EditJournal").hide(); 
+      $("#JournalList").show(); 
+    });
+
+    $("#saveEdit").off("click").on("click", function() {
+      const journalId = $(this).data("id");
+      const GETJOURNAL = RIJ1 + journalId + RIJ2;
+      console.log("GET URL:", GETJOURNAL);
+      console.log("Received data:", data);
+    
+      // Fetch current post details (if needed for confirmation or fallback)
+      $.getJSON(GETJOURNAL, function(data) {
+        const updatedData = {
+          id: journalId,
+          filePath: data.filePath,
+          fileLocator: journalId,
+          userID: data.userID,
+          title: $("#editJournalName").val() || data.journalName,
+        };
+
+    
+        // Send the PUT request
+        $.ajax({
+          type: "PUT",
+          url: UIJ1 + journalId + UIJ2,
+          data: JSON.stringify(updatedData),
+          contentType: "application/json",
+          success: function() {
+            alert("Journal updated successfully!");
+            console.log("GET URL:", GETJOURNAL);
+            console.log("Received data:", data);
+            getJournals();
+            $("#EditJournal").hide();
+            $("#JournalList").show(); 
+          },
+          error: function() {
+            alert("Failed to update the Journal.");
+          },
+        });
+      });
+    });
+    
+  
+
   });
+}
+
+function deleteJournal(id) {
+  $.ajax({
+    type: "DELETE",
+    url: DIJ1 + id + DIJ2,
+  })
+    .done(function() {
+      alert("Journal deleted successfully!");
+      getJournals();
+    })
 }
